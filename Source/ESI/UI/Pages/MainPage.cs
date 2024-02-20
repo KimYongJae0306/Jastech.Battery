6a2dc73d00a878fa07a848b70dc1f527cf0d15d9
Kim YoungJin <oz1995@jastech.co.kr>
2024-02-20 15:19:13 +0900

    - Test함수 Delay 주석 해제

--- Source/ESI/UI/Pages/MainPage.cs
+++ Source/ESI/UI/Pages/MainPage.cs
@@ -312,7 +312,7 @@ namespace ESI.UI.Pages
                         UpperDrawBoxControl.FitZoom();
                         LowerDrawBoxControl.FitZoom();
 
-                        //await Task.Delay(50);
+                        await Task.Delay(50);
                     }
                 }
                 catch (Exception ex)
